f7fb58d60152c1ecc913b935c009db7812b669d8
mdosmangoni6355 <mdosmangoni6355@gmail.com>
2023-09-24 18:26:42 +0600

    use state hooks and many more

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Posts from './Posts'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Core Concepts</h1>
+      <ol>
+        <li>Components</li>
+        <li>JSX</li>
+        <li>Props</li>
+        <li>Event Handler</li>
+        <li>State</li>
+      </ol>
+      <hr />
+      <Posts></Posts>
+    </>
+  )
+}
+
+export default App
---
+++ src/Posts.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import Post from "./Post"
+
+export default function Posts(){
+    const [posts, setPosts] = useState([])
+
+    useEffect( () =>{
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => setPosts(data))
+    }, [])
+    return (
+        <div className="">
+            <h3>Posts: {posts.length}</h3>
+            {
+                posts.map(post => <Post post={post}></Post>)
+            }
+        </div>
+    )
+}
+/**
+ * 1. create a state to store the data
+ * 2. create useEffect with no dependencies
+ * 3. use fetch to load data
+ */
